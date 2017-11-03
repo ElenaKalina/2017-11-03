ca8e5458a6ed00b642bec533423c3c457e2593b1
Student <student@lan.itstep.kiev.ua>
2017-11-03 19:39:57 +0200

    1

---
+++ script.js
@@ -0,0 +1,74 @@
+var z=0;
+var y=0;
+var a=confirm ("1+1=2?")
+var b=confirm ("1+2=3?")
+var c=confirm ("1+3=4?")
+var d=confirm ("1+4=5?")
+var e=confirm ("1+5=6?")
+
+
+if (a==true){z=z+1} else{y=y+1};
+if (b==true){z=z+1} else{y=y+1};
+if (c==true){z=z+1} else{y=y+1};
+if (d==true){z=z+1} else{y=y+1};
+if (e==true){z=z+1} else{y=y+1};
+
+alert ("правильных " + z + " Неправильных " + y)
+
+// if (a>b) {alert ("первое число (a) больше")}
+// else{alert ("второе число (b) больше")}
+
+// // Сравнение чисел
+
+// var a=+prompt ("ввод числа 1", 0)
+// var b=+prompt ("ввод числа 2", 0)
+// var c=+prompt ("ввод числа 3", 0)
+
+// if (a>b && b>c) {alert ("a")}
+// if (a<b && b>c) {alert ("b")}
+// if (a<b && b<c) {alert ("c")}
+// if (a==b && b==c) {alert ("равны")}
+
+
+
+
+
+
+
+
+
+// // Задание 4
+
+// var a=+prompt ("Введите номер квартиры от 1до 80")
+// if (a>0 && a<21) {alert ("Подъезд 1")}
+// if (a>20 && a<65) {alert ("Подъезд 2")}
+// if (a>64 && a<81) {alert ("Подъезд 3")}
+
+
+
+// задание 1
+// var a=prompt("ввод числа", 0);
+
+// if (a==10){alert ("Верно")}
+// else{alert ("Неверно")};
+
+
+// console.log(a)
+
+// var a=confirm("Ты студент?");
+
+
+// var a=+prompt ("ввод числа 1", 0)
+// var b=+prompt ("ввод числа 2", 0)
+
+// if (a>b) {alert ("первое число (a) больше")}
+// else{alert ("второе число (b) больше")}
+
+// console.log(a,b)
+// if(a==true){
+	// alert ('Hello student')
+// }
+// else {
+	// alert ("Скоро в армию")
+// }
+	
